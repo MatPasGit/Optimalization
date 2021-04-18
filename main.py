b1c4729cf0652ed95e32279eb3b2fcbfd61031be
pelswick100 <pelswick100@wp.pl>
2021-04-18 21:26:14 +0200

    Added generation of dataset

--- main.py
+++ main.py
@@ -5,22 +5,34 @@ from functions import *
 from BB import *
 from BS import *
 from RandomNumberGenerator import *
+import numpy as np
 
 #global vars
 problem_size=20
 
+
+def generatorInstancji(Z,n):
+    generator = RandomNumberGenerator(Z)
+    d = []
+    d = [[0 for i in range(n)] for j in range(n)]
+    w = []
+    w = [[0 for i in range(n)] for j in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if (i > j):
+                d[i][j] = generator.nextInt(1, 50)
+                d[j][i] = d[i][j]
+                w[i][j] = generator.nextInt(1, 50)
+                w[j][i] = w[i][j]
+    return w,d
+
 if __name__ == '__main__':
-    print(init_problem_instance(problem_size))
+    w,d = generatorInstancji(20, 5)
+    print(np.matrix(w))
+    print(np.matrix(d))
 
-
-#Z = int(input('Podaj ziarno(Z): '))
-#Z = 20
-#generator = RandomNumberGenerator(Z)
-
-#r = generator.nextInt(1, 20)
-#S[i] = r
-#j = generator.nextInt(0, m-1)
-#D[j] = D[j] + r
-
-
-#Control Commit
+#implementację metody dokładnej(B & B) dla zadanego problemu(maksymalnie + 2.0 do oceny),
+#implementację metody przybliżonej(BS) dla zadanego problemu(maksymalnie + 1.0 do oceny),
+#jakość dolnych i górnych ograniczeń(maksymalnie + 1.0 do oceny),
+#proste badania porównawcze(jakość/wydajność/zakres zastosowania) obu algorytmów(maksymalnie + 1.0 do oceny).
