7969eee3fd92ea0e819bcdee68a05324c601fc03
pelswick100 <pelswick100@wp.pl>
2021-04-18 20:54:43 +0200

    Added RandomNumberGenerator

--- main.py
+++ main.py
@@ -4,12 +4,23 @@
 from functions import *
 from BB import *
 from BS import *
+from RandomNumberGenerator import *
 
 #global vars
-problem_size=20;
+problem_size=20
 
 if __name__ == '__main__':
     print(init_problem_instance(problem_size))
+
+
+#Z = int(input('Podaj ziarno(Z): '))
+#Z = 20
+#generator = RandomNumberGenerator(Z)
+
+#r = generator.nextInt(1, 20)
+#S[i] = r
+#j = generator.nextInt(0, m-1)
+#D[j] = D[j] + r
 
 
 #Control Commit
