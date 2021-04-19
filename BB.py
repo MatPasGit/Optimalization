df163c122c147bf186a72db5a35609743b62683a
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2021-04-19 22:21:36 +0200

    first class concept

--- BB.py
+++ BB.py
@@ -1,9 +1,29 @@
-
+#imports
+import queue
+from functions import *
 
 class BranchAndBound:
 
-    def countLowerBound(self):
-        return 0
+    instance = []
+    flowList =[]
+    distanceList=[]
+
+    lowerBound = 0
+    upperBound =0
+    resolutionsQueue = queue.LifoQueue()
+
+    #methods
+    def BranchAndBound(self, initRandomInstance,w,d):
+        self.instance = initRandomInstance
+        self.flowList = w
+        self.distanceList = d
+        self.initLowerBound()
+
+
+    def initLowerBound(self):
+        self.lowerBound = objective_function(self.instance,self.flowList,self.distanceList)
+
+    def initUpperBound(self):
 
     def countUpperBound(self):
         return 0
