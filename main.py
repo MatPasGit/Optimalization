2631d8b9b67a3f101aa760f3a2f89317028e060b
pelswick100 <pelswick100@wp.pl>
2021-04-20 23:32:02 +0200

    Added statistics

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from BB import *
 from BS import *
 from RandomNumberGenerator import *
 import numpy as np
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import time
+
 
 #global vars
 problem_size=9
@@ -27,24 +31,119 @@ def generatorInstancji(Z,n):
     return w,d
 
 if __name__ == '__main__':
-    #w - macierz przeplywów
-    #d - macierz odległości
-    w,d = generatorInstancji(20, problem_size)
-    #print(np.matrix(w))
 
-    lets_solve = BranchAndBound(w, d)
-    lets_solve.solve()
+    wynikiBBtime=[]
+    wynikiBStime=[]
+    wynikiBBpoints=[]
+    wynikiBSpoints=[]
 
-    print("resolution:" , lets_solve.upperBound)
-    print("instance: ", lets_solve.instance)
-    print("how much similar results? : ", lets_solve.same_value_solution)
+    zakres=7
+    for problem_size in range(3,3+zakres):
 
-    lets_solveBS = BS(w, d)
-    lets_solveBS.solve()
+        #w - macierz przeplywów
+        #d - macierz odległości
+        w,d = generatorInstancji(20, problem_size)
+        #print(np.matrix(w))
 
-    print("BS resolution:" , lets_solveBS.upperBound)
-    print("BS instance: ", lets_solveBS.instance)
-    print("BS how much similar results? : ", lets_solveBS.same_value_solution)
+        lets_solve = BranchAndBound(w, d)
+        start = time.time()
+        lets_solve.solve()
+        end = time.time()
+        wynikiBBtime.append(end - start)
+        wynikiBBpoints.append(lets_solve.upperBound)
+
+        print("resolution:" , lets_solve.upperBound)
+        print("instance: ", lets_solve.instance)
+        print("how much similar results? : ", lets_solve.same_value_solution)
+
+        lets_solveBS = BS(w, d)
+        start = time.time()
+        lets_solveBS.solve()
+        end = time.time()
+        wynikiBStime.append(end - start)
+        wynikiBSpoints.append(lets_solveBS.upperBound)
+
+        print("BS resolution:" , lets_solveBS.upperBound)
+        print("BS instance: ", lets_solveBS.instance)
+        print("BS how much similar results? : ", lets_solveBS.same_value_solution)
+
+    argumenty = np.linspace(3,3+zakres,zakres)
+
+    plt.plot(argumenty, wynikiBBtime, label="B&B")
+    plt.plot(argumenty, wynikiBStime, label="BS")
+    plt.grid(True)
+    plt.xlabel("Rozmiar problemu (n)")
+    plt.ylabel("Czas [s]")
+    plt.title("Wykres czasu rozwiązania problemu dla stałego ziarna")
+    plt.legend()
+    plt.savefig("WykresCzasuStaleZiarno.jpg", dpi=72)
+    plt.show()
+
+    plt.plot(argumenty, wynikiBBpoints, label="B&B")
+    plt.plot(argumenty, wynikiBSpoints, label="BS")
+    plt.grid(True)
+    plt.xlabel("Rozmiar problemu (n)")
+    plt.ylabel("Wartość funkcji celu")
+    plt.title("Wykres jakości rozwiązania problemu dla stałego ziarna")
+    plt.legend()
+    plt.savefig("WykresJakosciStaleZiarno.jpg", dpi=72)
+    plt.show()
+
+    wynikiBBtime.clear()
+    wynikiBStime.clear()
+    wynikiBBpoints.clear()
+    wynikiBSpoints.clear()
+    Z = RandomNumberGenerator(100)
+
+    for problem_size in range(3, 3+zakres):
+
+        #w - macierz przeplywów
+        #d - macierz odległości
+        w, d = generatorInstancji(Z.nextInt(10,100), problem_size)
+        #print(np.matrix(w))
+
+        lets_solve = BranchAndBound(w, d)
+        start = time.time()
+        lets_solve.solve()
+        end = time.time()
+        wynikiBBtime.append(end - start)
+        wynikiBBpoints.append(lets_solve.upperBound)
+
+        print("resolution:", lets_solve.upperBound)
+        print("instance: ", lets_solve.instance)
+        print("how much similar results? : ", lets_solve.same_value_solution)
+
+        lets_solveBS = BS(w, d)
+        start = time.time()
+        lets_solveBS.solve()
+        end = time.time()
+        wynikiBStime.append(end - start)
+        wynikiBSpoints.append(lets_solveBS.upperBound)
+
+        print("BS resolution:", lets_solveBS.upperBound)
+        print("BS instance: ", lets_solveBS.instance)
+        print("BS how much similar results? : ",
+              lets_solveBS.same_value_solution)
+
+    plt.plot(argumenty, wynikiBBtime, label="B&B")
+    plt.plot(argumenty, wynikiBStime, label="BS")
+    plt.grid(True)
+    plt.xlabel("Rozmiar problemu (n)")
+    plt.ylabel("Czas [s]")
+    plt.title("Wykres czasu rozwiązania problemu dla zmiennego ziarna")
+    plt.legend()
+    plt.savefig("WykresCzasuZmienneZiarno.jpg", dpi=72)
+    plt.show()
+
+    plt.plot(argumenty, wynikiBBpoints, label="B&B")
+    plt.plot(argumenty, wynikiBSpoints, label="BS")
+    plt.grid(True)
+    plt.xlabel("Rozmiar problemu (n)")
+    plt.ylabel("Wartość funkcji celu")
+    plt.title("Wykres jakości rozwiązania problemu dla zmiennego ziarna")
+    plt.legend()
+    plt.savefig("WykresJakosciZmienneZiarno.jpg", dpi=72)
+    plt.show()
 
 #implementację metody dokładnej(B & B) dla zadanego problemu(maksymalnie + 2.0 do oceny),
 #implementację metody przybliżonej(BS) dla zadanego problemu(maksymalnie + 1.0 do oceny),
