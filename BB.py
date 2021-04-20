f15e78ed216c4191c963765635e1ac4dc83561de
MatPasGit <47747551+MatPasGit@users.noreply.github.com>
2021-04-20 20:18:17 +0200

    I think we've finished (testing time)

--- BB.py
+++ BB.py
@@ -9,8 +9,8 @@ class BranchAndBound:
     flowList =[]
     distanceList=[]
     size = 0
+    same_value_solution=0
 
-    lowerBound = 0
     upperBound =0
     resolutionsQueue = queue.LifoQueue()
 
@@ -52,14 +52,10 @@ class BranchAndBound:
 
     def solve(self):
 
-
-
-        #Queue
-
         #inicjowanie potencjalnie najlepszego rozwiazania
         self.initUpperBound()
 
-        for x in range(0, self.size ):
+        for x in range(0, self.size ): #dodaj pierwsze node'y
             temp_instance=Node([x], objective_function([x],self.flowList, self.distanceList))
             self.resolutionsQueue.put(temp_instance)
 
@@ -70,19 +66,24 @@ class BranchAndBound:
             # if is leave
             if(len(instance.node_list) == self.size): #jesli jest lisciem
 
-                if instance.value < self.upperBound :
+                if instance.value < self.upperBound : # jezeli rozwiazanie lepsze to je zapisz
                     self.instance = instance.node_list
-                    self.upperBound  = instance.value
+                    self.upperBound = instance.value
+                    self.same_value_solution = 0
                     print("New value: ", self.upperBound, "New best sequence", self.instance)
-            else:
+                if instance.value == self.upperBound:
+                    self.same_value_solution += 1
+
+            else: ##jeśli nie jest liściem
                 for x in range(0,self.size):
-                    if x in instance: #jesli zaklad zostal juz przydzielony
+                    if x in instance: #jesli zaklad zostal juz przydzielony pomiń
                         continue
+
                     child_list = instance.node_list
                     child_list.append()
                     child = Node(child_list, objective_function(child_list,self.flowList, self.distanceList))
-                    self.resolutionsQueue.put()
-
-
+                    if child.value > self.upperBound: ##jeśli wartość tymczasowego rozwiazanie jest wieksza od upperBound nie rozwijaj drzewa dalej
+                        continue
+                    self.resolutionsQueue.put(child)
 
         return 0
